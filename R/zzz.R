2ad34140cbeb5e9130ec5e8ab323117143a56454
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-06 17:46:27 +0000

    more RSQLite-related changes
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DECIPHER@95003 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,10 @@
 .onUnload <- function(libpath)
     library.dynam.unload("DECIPHER", libpath)
+
+dbIsValid <- function(...)
+{
+    if ("dbIsValid" %in% getNamespace)
+        RSQLite::dbIsValid(...)
+    else
+        RSQLite::isIdCurrent(...)
+}
