9b3155325bb566a241ec074e2ae3fb2a5e47d777
e.wright <e.wright@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-07 10:14:51 +0000

    Small change to zzz.R.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DECIPHER@95054 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,16 +3,16 @@
 
 dbIsValid <- function(...) {
 	if (packageVersion("RSQLite") >= package_version("1.0.0")) {
-		do.call("dbIsValid", as.list(...), envir=getNamespace("RSQLite"))
+		do.call("dbIsValid", list(...), envir=getNamespace("RSQLite"))
 	} else {
-		do.call("isIdCurrent", as.list(...), envir=getNamespace("RSQLite"))
+		do.call("isIdCurrent", list(...), envir=getNamespace("RSQLite"))
 	}
 }
 
 dbBegin <- function(...) {
 	if (packageVersion("RSQLite") >= package_version("1.0.0")) {
-		do.call("dbBegin", as.list(...), envir=getNamespace("RSQLite"))
+		do.call("dbBegin", list(...), envir=getNamespace("RSQLite"))
 	} else {
-		do.call("dbBeginTransaction", as.list(...), envir=getNamespace("RSQLite"))
+		do.call("dbBeginTransaction", list(...), envir=getNamespace("RSQLite"))
 	}
 }
